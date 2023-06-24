352e7938e95863a54048f22c4165d01797a3d38f
pradeepmummaneni <pradeepchow850@gmail.com>
2023-06-23 20:11:11 -0400

    functionchnages

---
+++ pradeep/app.js
@@ -0,0 +1,60 @@
+
+// Data types in Java Script
+ // Primitive data type
+
+ var a =10;
+ var b  = 20;
+  
+ var sum = a+b;
+ console.log(sum);
+   
+ // decimals
+
+ var a = 20.99;
+ var b = 30.11;
+  var sum = a+b;
+  console.log(sum);
+
+// String
+
+var AB = "Pradeep"
+var BC = " Sai"
+ var print =AB+BC;
+ console.log(print);
+
+ //want to see what type of data type
+
+ var PR = "Pradeep Mummaneni"
+
+ console.log(typeof PR);
+
+ // Boolean
+
+ var A = 25;
+ var B = 30;
+  console.log(A>B) // greater than
+  console.log(A<B); // less than
+  console.log(A==B); // equals to
+  console.log(typeof A);
+  
+  // Undefined
+
+  var Jaisai;
+  console.log(Jaisai);
+
+  // null 
+
+  var Sai = null;
+  console.log(Sai);
+
+
+  var myarray =[11,22,33]
+  myarray [0] =1001;
+  console.log(myarray);
+
+
+
+   
+
+
+
---
+++ pradeep/functions.js
@@ -0,0 +1,73 @@
+
+// function
+
+  function num(){
+    var A =10;
+    var B=20;
+    return A+B;
+    }
+    var val =num();
+    console.log(val);
+
+
+// Substraction
+ function sub(){
+    var p =10;
+    var r =20;
+    return p-r;
+ }
+    var val =sub();
+    console.log(val);
+ 
+
+ //multiplication
+
+  function mul(){
+    var b =25;
+    var c =35;
+    return b*c;
+ }
+    var val =mul();
+    console.log(val);
+
+    // Divison
+    function div(){
+        var b=10;
+        var c=2;
+        return b/c;
+    }
+    var val =div();
+    console.log(val);
+
+
+    // percentage
+
+    function percentage(num,per) {
+    
+    return (num/100)*per;
+    }
+    console.log(percentage(1000,25));
+
+
+
+
+//functions inside function for Divison
+ var a =myFunction(25,35);
+ function myFunction(c,b){
+    return c/b;
+ }
+     console.log(a);
+
+
+     // Functions inside function
+  
+  var x =myFunction(10,25);
+    
+    function myFunction(k,p){
+      return k+p;
+    
+    }
+      console.log(x);
+
+
+    
