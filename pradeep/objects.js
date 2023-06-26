60a2621c701cde4ce4ee6e8b0b3fec8b12817ba1
pradeepmummaneni <pradeepchow850@gmail.com>
2023-06-26 15:39:58 -0400

    Employee and  Cars bojects

--- pradeep/objects.js
+++ pradeep/objects.js
@@ -108,3 +108,73 @@ return this.fname+' '+this.lname;
     // Any Parent we create  every object is  called Object
 // Difference between Class based inhertance and Prototype inheritance
 
+
+
+// Decalring Object in JS
+
+var cars = {
+
+brand: 'Tesla',
+model :' 2023',
+electric : 'yes',
+country :'Usa',
+market :'Global',
+        }
+// Reading obkects 
+
+console.log(cars.brand); // dot notation
+cars.location="Charlotte";  // Adding or assiging new property to already created object.
+console.log(cars.location);
+
+
+var business = 'country'   // bracket noattion 
+console.log(cars[business]);  // usage is prints dynamically.
+
+// constructor function
+
+
+function Cars( brand,model,electric,country,market){
+
+    this.brand =brand;
+    this.model= model;
+    this.electric= electric;
+    this.country = country;
+    this.market = market;
+   
+    }
+
+
+var cars2 =new Cars('fTesla',2023,'Yes','lUsa','Global');
+console.log(cars2);
+var cars1 =new Cars('Bmw',2024,'No','INDIA','Global');
+console.log(cars1);
+
+
+// Inheritance Prototype
+
+function Cars(fname,lname,brand,model,electric,country,market){
+
+    this.fname= fname;
+    this.lname=lname;
+    this.brand = brand;
+    this.model=model;
+    this.electric= electric;
+    this.country=country;
+    this.market=market;
+    Cars.prototype.owner ='Pradeep';
+    Cars.prototype.getFullName = function(){     // Prototype
+        return this.fname +''+this.lname;
+
+    }
+}
+var cars =new Cars('Range', 'Rover','Tata','2023','yes','India','Global');
+console.log(cars.getFullName()); 
+console.log(cars);
+console.log(cars.owner);
+
+
+
+
+
+
+
