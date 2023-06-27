4cdf41e1a25302d4a08fdd2dafee7bd9a6e16400
bhavika <bhavika5023@gmail.com>
2023-06-27 17:04:18 -0400

    conditions and loops

---
+++ bhavika/dailywork/app_jun27_2.js
@@ -0,0 +1,68 @@
+
+////If condition
+var x=10;
+var y=20;
+if(x>y){
+    console.log('x is greater than y');
+
+}
+else
+{
+    console.log('y is greater than x');
+}
+
+////else if condition
+
+var z=30;
+if(x>y && x>z ){
+    console.log('x is greatest of three numbers');
+
+}
+else if (y>x && y>z)
+{
+    console.log('y is greatest of three numbers');
+}
+else
+{
+    console.log('z is greatest of three numbers');
+}
+
+// ///switch
+// var fruits= "Apple";
+
+// switch(fruits) {
+ 
+// case   "Banana":
+//        alert("Hello")
+//        break;
+    
+// case "Apple":
+//        alert("Welcome")
+//        break;  
+       
+// default:
+//     alert ("server down")
+//    }
+
+///for loop
+
+//for( var i=0; i<10; i++)
+//{
+  //  console.log("Hello world"+' '+i);
+//}
+
+//while loop
+//var x=20;
+//while(x>10){
+  //  console.log("success");
+//}
+
+
+// do while
+var x =20;
+do{
+    
+    console.log("success");
+}
+
+while(x>10);
